3130c8cfc97b98634c3d980a1c9d774cff081bde
ArthurYurkov <77395695+ArthurYurkov@users.noreply.github.com>
2023-01-18 16:25:14 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,16 @@
-import { Routes, Route } from 'react-router-dom';
-import { lazy, Suspense } from 'react';
-
-import Layout from './Layout/Layout';
-
-const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
-const Movies = lazy(() => import('../pages/Movies/Movies'));
-const MoviePageDetails = lazy(() =>
-  import('../pages/MoviePageDetails/MoviePageDetails')
-);
-const Cast = lazy(() => import('../pages/Cast/Cast'));
-const Reviews = lazy(() => import('../pages/Reviews/Reviews'));
-
-export default function App() {
+export const App = () => {
   return (
-    <>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route path="/" element={<HomePage />} />
-            <Route path="movies" element={<Movies />} />
-            <Route path="movies/:movieId" element={<MoviePageDetails />}>
-              <Route path="cast" element={<Cast />} />
-              <Route path="reviews" element={<Reviews />} />
-            </Route>
-          </Route>
-        </Routes>
-      </Suspense>
-    </>
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
   );
-}
+};
