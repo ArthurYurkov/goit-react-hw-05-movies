68f07742bba2f5fa1e61cb18c01e89d2cfc2007b
ArthurYurkov <77395695+ArthurYurkov@users.noreply.github.com>
2023-01-18 16:25:41 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
-export const App = () => {
+import { Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+
+import Layout from './Layout/Layout';
+
+const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
+const Movies = lazy(() => import('../pages/Movies/Movies'));
+const MoviePageDetails = lazy(() =>
+  import('../pages/MoviePageDetails/MoviePageDetails')
+);
+const Cast = lazy(() => import('../pages/Cast/Cast'));
+const Reviews = lazy(() => import('../pages/Reviews/Reviews'));
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="movies" element={<Movies />} />
+            <Route path="movies/:movieId" element={<MoviePageDetails />}>
+              <Route path="cast" element={<Cast />} />
+              <Route path="reviews" element={<Reviews />} />
+            </Route>
+          </Route>
+        </Routes>
+      </Suspense>
+    </>
   );
-};
+}
